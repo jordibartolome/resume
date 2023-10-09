6318504053e6eda193ec0269aade72b15dcd81fd
Jordi Bartolomé <jordi@noteflight.com>
2023-10-08 22:18:06 -0700

    Add prettier

--- src/data.tsx
+++ src/data.tsx
@@ -1,4 +1,4 @@
-import { DateTime } from "luxon";
+import {DateTime} from 'luxon';
 import {
   IProject,
   IJob,
@@ -6,85 +6,85 @@ import {
   INetwork,
   ILanguage,
   IPublication,
-  ICourse
-} from "./types";
+  ICourse,
+} from './types';
 
-const DATE_FORMAT = "dd-MM-yyyy";
+const DATE_FORMAT = 'dd-MM-yyyy';
 export const RESUME_LINK =
-  "https://docs.google.com/document/d/16Fkdy_C7LjiDxMtol0pBb7x8EW2xeLKpwzO7CcCRt3k/edit#heading=h.nrl5r7tqva24";
+  'https://docs.google.com/document/d/16Fkdy_C7LjiDxMtol0pBb7x8EW2xeLKpwzO7CcCRt3k/edit#heading=h.nrl5r7tqva24';
 
 export const PROJECTS: IProject[] = [
   {
-    id: "google",
-    name: "Google",
-    logoUrl: "/images/google.jpg",
-    description: "Google Assistant team.",
-    link: "https://www.google.com/"
+    id: 'google',
+    name: 'Google',
+    logoUrl: '/images/google.jpg',
+    description: 'Google Assistant team.',
+    link: 'https://www.google.com/',
   },
   {
-    id: "amazon",
-    name: "Amazon Web Services",
-    logoUrl: "/images/aws.png",
-    description: "AWS Detective, a new cybersecurity service.",
-    link: "https://www.aws.com/detective"
+    id: 'amazon',
+    name: 'Amazon Web Services',
+    logoUrl: '/images/aws.png',
+    description: 'AWS Detective, a new cybersecurity service.',
+    link: 'https://www.aws.com/detective',
   },
   {
-    id: "noteflight",
-    name: "Noteflight",
-    logoUrl: "/images/noteflight.png",
+    id: 'noteflight',
+    name: 'Noteflight',
+    logoUrl: '/images/noteflight.png',
     description:
-      "The future of online music notation. An online music notation editor to create, transcribe and play classical music.",
-    link: "http://www.noteflight.com"
+      'The future of online music notation. An online music notation editor to create, transcribe and play classical music.',
+    link: 'http://www.noteflight.com',
   },
   {
-    id: "whatameal",
-    name: "Whatameal",
-    logoUrl: "/images/whatameal.png",
-    description: "Share and create recipes online.",
-    link: "https://www.whatameal.com"
+    id: 'whatameal',
+    name: 'Whatameal',
+    logoUrl: '/images/whatameal.png',
+    description: 'Share and create recipes online.',
+    link: 'https://www.whatameal.com',
   },
   {
-    id: "volotea",
-    name: "Volotea",
-    logoUrl: "/images/volotea.png",
-    description: "A new European airline. ",
-    link: "https://www.volotea.com/en"
+    id: 'volotea',
+    name: 'Volotea',
+    logoUrl: '/images/volotea.png',
+    description: 'A new European airline. ',
+    link: 'https://www.volotea.com/en',
   },
   {
-    id: "me",
-    name: "jordibartolome.me",
-    logoUrl: "/images/jordibartolome.png",
-    description: "This site! Feel free to fork it on github.",
-    link: "/"
+    id: 'me',
+    name: 'jordibartolome.me',
+    logoUrl: '/images/jordibartolome.png',
+    description: 'This site! Feel free to fork it on github.',
+    link: '/',
   },
   {
-    id: "music21",
-    name: "music21",
-    logoUrl: "/images/music21.png",
+    id: 'music21',
+    name: 'music21',
+    logoUrl: '/images/music21.png',
     description:
-      "A python based toolkit for computed-aided musicology. It has never been easier to analyze music.",
-    link: "http://web.mit.edu/music21/"
-  }
+      'A python based toolkit for computed-aided musicology. It has never been easier to analyze music.',
+    link: 'http://web.mit.edu/music21/',
+  },
 ];
 
 export const JOBS: IJob[] = [
   {
-    title: "Senior Software Engineer",
-    company: "Google",
-    link: "https://www.google.com",
-    city: "Mountain View, CA, United States",
-    startDate: DateTime.fromFormat("22-04-2021", DATE_FORMAT),
+    title: 'Senior Software Engineer',
+    company: 'Google',
+    link: 'https://www.google.com',
+    city: 'Mountain View, CA, United States',
+    startDate: DateTime.fromFormat('22-04-2021', DATE_FORMAT),
     description:
-      "Worked on Google Assistant. Understanding how Assistant and Bard queries are executed.",
-    bullets: []
+      'Worked on Google Assistant. Understanding how Assistant and Bard queries are executed.',
+    bullets: [],
   },
   {
-    title: "Front End Engineer",
-    company: "Amazon Web Services",
-    link: "https://www.aws.com",
-    city: "Cambridge, MA, United States",
-    startDate: DateTime.fromFormat("22-07-2019", DATE_FORMAT),
-    endDate: DateTime.fromFormat("19-04-2021", DATE_FORMAT),
+    title: 'Front End Engineer',
+    company: 'Amazon Web Services',
+    link: 'https://www.aws.com',
+    city: 'Cambridge, MA, United States',
+    startDate: DateTime.fromFormat('22-07-2019', DATE_FORMAT),
+    endDate: DateTime.fromFormat('19-04-2021', DATE_FORMAT),
     description:
       "Worked on the development and release of <a href='https://www.aws.com' class='regularLink' target='_blank'>AWS Detective</a>, a new AWS cybersecurity service. ",
     bullets: [
@@ -92,195 +92,193 @@ export const JOBS: IJob[] = [
       // "Implemented React/Redux.",
       // "Development of a real time audio transcription using Machine Learning with BrainJS.",
       // "Built a whole new commerce system."
-    ]
+    ],
   },
   {
-    title: "Principal Software Engineer",
-    company: "Noteflight",
-    link: "https://www.noteflight.com",
-    city: "Somerville, MA, United States",
-    startDate: DateTime.fromFormat("04-11-2013", DATE_FORMAT),
-    endDate: DateTime.fromFormat("19-07-2019", DATE_FORMAT),
+    title: 'Principal Software Engineer',
+    company: 'Noteflight',
+    link: 'https://www.noteflight.com',
+    city: 'Somerville, MA, United States',
+    startDate: DateTime.fromFormat('04-11-2013', DATE_FORMAT),
+    endDate: DateTime.fromFormat('19-07-2019', DATE_FORMAT),
     description:
       "<a href='http://www.noteflight.com' class='regularLink' target='_blank'>Noteflight</a> is a heavy Javascript app to write musical scores online. Worked on the full scope of Noteflight's software stack, from the frontend to the backend. Led and mentored a team of developers and worked on the design and prioritization of the projects.",
     bullets: [
-      "Optimization of the heavy Javascript Noteflight Editor, improving loading and editing algorithms. ",
-      "Implemented React/Redux.",
-      "Development of a real time audio transcription using Machine Learning with BrainJS.",
-      "Built a whole new commerce system."
-    ]
+      'Optimization of the heavy Javascript Noteflight Editor, improving loading and editing algorithms. ',
+      'Implemented React/Redux.',
+      'Development of a real time audio transcription using Machine Learning with BrainJS.',
+      'Built a whole new commerce system.',
+    ],
   },
   {
-    title: "Web consultant",
-    company: "Newshore",
-    link: "http://www.newshore.es",
-    city: "Barcelona, Catalonia",
-    startDate: DateTime.fromFormat("04-05-2012", DATE_FORMAT),
-    endDate: DateTime.fromFormat("28-10-2013", DATE_FORMAT),
+    title: 'Web consultant',
+    company: 'Newshore',
+    link: 'http://www.newshore.es',
+    city: 'Barcelona, Catalonia',
+    startDate: DateTime.fromFormat('04-05-2012', DATE_FORMAT),
+    endDate: DateTime.fromFormat('28-10-2013', DATE_FORMAT),
     description:
       "Development of <a href='http://www.volotea.com' class='regularLink' target='_blank'>Volotea</a>. A new European airline. Umbraco, .NET, Javascript and CSS.",
     bullets: [
-      "Designed part of the architecture of the Content Management System (CMS) and optimized the performance of the web.",
-      "Also designed different windows services and web services related to the website."
-    ]
+      'Designed part of the architecture of the Content Management System (CMS) and optimized the performance of the web.',
+      'Also designed different windows services and web services related to the website.',
+    ],
   },
   {
-    title: "Consultant",
-    company: "Better Consultants",
-    link: "http://www.betterconsultants.es",
-    city: "Barcelona, Catalonia",
-    startDate: DateTime.fromFormat("04-10-2011", DATE_FORMAT),
-    endDate: DateTime.fromFormat("02-05-2012", DATE_FORMAT),
+    title: 'Consultant',
+    company: 'Better Consultants',
+    link: 'http://www.betterconsultants.es',
+    city: 'Barcelona, Catalonia',
+    startDate: DateTime.fromFormat('04-10-2011', DATE_FORMAT),
+    endDate: DateTime.fromFormat('02-05-2012', DATE_FORMAT),
     description:
-      "Development of software for the national police. Visual Basic, Cobol and SQL.",
+      'Development of software for the national police. Visual Basic, Cobol and SQL.',
     bullets: [
-      "Software analyzer and developer of Business Intelligence applications for the national police department."
-    ]
+      'Software analyzer and developer of Business Intelligence applications for the national police department.',
+    ],
   },
   {
-    title: "Researcher",
-    company: "Massachusetts Institute of Technology",
-    link: "http://web.mit.edu/music21/",
-    city: "Cambridge, MA, United States",
-    startDate: DateTime.fromFormat("01-06-2011", DATE_FORMAT),
-    endDate: DateTime.fromFormat("12-09-2011", DATE_FORMAT),
+    title: 'Researcher',
+    company: 'Massachusetts Institute of Technology',
+    link: 'http://web.mit.edu/music21/',
+    city: 'Cambridge, MA, United States',
+    startDate: DateTime.fromFormat('01-06-2011', DATE_FORMAT),
+    endDate: DateTime.fromFormat('12-09-2011', DATE_FORMAT),
     description:
       "Development of <a href='http://web.mit.edu/music21/' class='regularLink' target='_blank'>music21</a>. Developed in Python.",
     bullets: [
-      "Participated in the “music21” project, a set of tools for helping scholars and other active listeners answer questions about music quickly and simply.",
-      "Developed a software that detects, in real time, through audio and signal processing techniques, the note that a musician plays and shows the score properly. Advisor: Dr. Michael Scott Cuthbert.",
+      'Participated in the “music21” project, a set of tools for helping scholars and other active listeners answer questions about music quickly and simply.',
+      'Developed a software that detects, in real time, through audio and signal processing techniques, the note that a musician plays and shows the score properly. Advisor: Dr. Michael Scott Cuthbert.',
       "The results of this project were published in 'New applications to Score Following and feature extraction beyond MIDI', by J. Bartolomé and M.S. Cuthbert (NIPS 2011 – Music and Machine Learning Workshop. December 2011, Granada, Spain)",
-      "Example of some developed applications <a class='regularLink' href='http://www.youtube.com/watch?v=h48hSl6syyw' target='_blank'>here</a>."
-    ]
-  }
+      "Example of some developed applications <a class='regularLink' href='http://www.youtube.com/watch?v=h48hSl6syyw' target='_blank'>here</a>.",
+    ],
+  },
 ];
 
 export const EDUCATIONS = [
   {
-    title: "Telecommunications Engineering - BSc and MSc",
-    university: "Polytechnic University of Catalonia",
-    startDate: DateTime.fromFormat("12-09-2005", DATE_FORMAT),
-    endDate: DateTime.fromFormat("28-06-2011", DATE_FORMAT),
+    title: 'Telecommunications Engineering - BSc and MSc',
+    university: 'Polytechnic University of Catalonia',
+    startDate: DateTime.fromFormat('12-09-2005', DATE_FORMAT),
+    endDate: DateTime.fromFormat('28-06-2011', DATE_FORMAT),
     description:
-      "Relevant coursework: Programming, Signal Processing, Telematics, Mathematics, Physics and Electronics."
+      'Relevant coursework: Programming, Signal Processing, Telematics, Mathematics, Physics and Electronics.',
   },
   {
-    title: "Master thesis",
-    university: "Pompeu Fabra University",
-    startDate: DateTime.fromFormat("01-01-2011", DATE_FORMAT),
-    endDate: DateTime.fromFormat("28-06-2011", DATE_FORMAT),
+    title: 'Master thesis',
+    university: 'Pompeu Fabra University',
+    startDate: DateTime.fromFormat('01-01-2011', DATE_FORMAT),
+    endDate: DateTime.fromFormat('28-06-2011', DATE_FORMAT),
     description:
-      "Development of Master’s Thesis titled: 'Alignment of the recording of a violin performance with the corresponding musical score', consisting on the development of a software to process the audio signal and the movement of the bow, in order to find the notes played by a violinist. Advisor: Dr. Xavier Serra."
+      "Development of Master’s Thesis titled: 'Alignment of the recording of a violin performance with the corresponding musical score', consisting on the development of a software to process the audio signal and the movement of the bow, in order to find the notes played by a violinist. Advisor: Dr. Xavier Serra.",
   },
   {
-    title: "Music - violin",
-    university: "Conservatori Municipal de Barcelona",
-    startDate: DateTime.fromFormat("01-09-1999", DATE_FORMAT),
-    endDate: DateTime.fromFormat("28-06-2006", DATE_FORMAT),
-    description: "Violin, harmony, orchestra... lots of years, lots of things!"
-  }
+    title: 'Music - violin',
+    university: 'Conservatori Municipal de Barcelona',
+    startDate: DateTime.fromFormat('01-09-1999', DATE_FORMAT),
+    endDate: DateTime.fromFormat('28-06-2006', DATE_FORMAT),
+    description: 'Violin, harmony, orchestra... lots of years, lots of things!',
+  },
 ];
 
 export const SKILLS: ISkillGroup[] = [
   {
-    id: "js",
-    group: "Javascript Technologies",
-    className: "jsTechnologies skillsGroupWrapper",
+    id: 'js',
+    group: 'Javascript Technologies',
+    className: 'jsTechnologies skillsGroupWrapper',
     elements: [
-      "Javascript ES6",
-      "React/Redux",
-      "Typescript",
-      "Web audio API",
-      "BrainJS",
-      "TensorFlowJS"
-    ]
+      'Javascript ES6',
+      'React/Redux',
+      'Typescript',
+      'Web audio API',
+      'BrainJS',
+      'TensorFlowJS',
+    ],
   },
   {
-    id: "languages",
-    group: "Programming Languages",
-    className: "programmingLanguages skillsGroupWrapper",
-    elements: ["Python", "Ruby on Rails", "Java", "C#"]
+    id: 'languages',
+    group: 'Programming Languages',
+    className: 'programmingLanguages skillsGroupWrapper',
+    elements: ['Python', 'Ruby on Rails', 'Java', 'C#'],
   },
   {
-    id: "web",
-    group: "Web Technologies",
-    className: "webTechnologies skillsGroupWrapper",
-    elements: ["Webpack", "CSS", "ElasticSearch", "AWS"]
+    id: 'web',
+    group: 'Web Technologies',
+    className: 'webTechnologies skillsGroupWrapper',
+    elements: ['Webpack', 'CSS', 'ElasticSearch', 'AWS'],
   },
   {
-    id: "other",
-    group: "Others",
-    className: "others skillsGroupWrapper",
-    elements: ["Git", "SEO"]
-  }
+    id: 'other',
+    group: 'Others',
+    className: 'others skillsGroupWrapper',
+    elements: ['Git', 'SEO'],
+  },
 ];
 
 export const LANGUAGES: ILanguage[] = [
   {
-    language: "Catalan",
-    level: "Native"
+    language: 'Catalan',
+    level: 'Native',
   },
   {
-    language: "Spanish",
-    level: "Native"
+    language: 'Spanish',
+    level: 'Native',
   },
   {
-    language: "English",
-    level: "Proficiency"
-  }
+    language: 'English',
+    level: 'Proficiency',
+  },
 ];
 
 export const PUBLICATIONS: IPublication[] = [
   {
     title:
-      "Symbolic Music Information Retrieval with music21: New Applications to Score Following and Feature Extraction Beyond MIDI.",
+      'Symbolic Music Information Retrieval with music21: New Applications to Score Following and Feature Extraction Beyond MIDI.',
     where: "NIPS'11 - Music Machine Learning",
-    date: DateTime.fromFormat("16-12-2011", DATE_FORMAT),
-    link:
-      "https://web.mit.edu/music21/papers/Bartolome-Guillen_Cuthbert_ScoreFollower.pdf"
-  }
+    date: DateTime.fromFormat('16-12-2011', DATE_FORMAT),
+    link: 'https://web.mit.edu/music21/papers/Bartolome-Guillen_Cuthbert_ScoreFollower.pdf',
+  },
 ];
 
 export const COURSES: ICourse[] = [
   {
-    title: "Machine Learning",
-    where: "Coursera",
-    date: DateTime.fromFormat("01-01-2018", DATE_FORMAT)
+    title: 'Machine Learning',
+    where: 'Coursera',
+    date: DateTime.fromFormat('01-01-2018', DATE_FORMAT),
   },
   {
-    title: "Artificial Intelligence: Neural Networks",
-    where: "LinkedIn Learning",
-    date: DateTime.fromFormat("01-01-2020", DATE_FORMAT)
+    title: 'Artificial Intelligence: Neural Networks',
+    where: 'LinkedIn Learning',
+    date: DateTime.fromFormat('01-01-2020', DATE_FORMAT),
   },
   {
-    title: "Learning TensorFlow with JS",
-    where: "LinkedIn Learning",
-    date: DateTime.fromFormat("01-02-2020", DATE_FORMAT)
-  }
+    title: 'Learning TensorFlow with JS',
+    where: 'LinkedIn Learning',
+    date: DateTime.fromFormat('01-02-2020', DATE_FORMAT),
+  },
 ];
 
 export const NETWORKS: INetwork[] = [
   {
-    className: "fa fa-fw fa-at",
-    link: "mailto:jordi.bartolome.guillen@gmail.com"
+    className: 'fa fa-fw fa-at',
+    link: 'mailto:jordi.bartolome.guillen@gmail.com',
   },
   {
-    className: "fa fa-fw fa-linkedin",
-    link:
-      "https://www.linkedin.com/pub/jordi-bartolom%C3%A9-guill%C3%A9n/35/667/533"
+    className: 'fa fa-fw fa-linkedin',
+    link: 'https://www.linkedin.com/pub/jordi-bartolom%C3%A9-guill%C3%A9n/35/667/533',
   },
   {
-    className: "fa fa-fw fa-github",
-    link: "https://github.com/jordibartolome"
+    className: 'fa fa-fw fa-github',
+    link: 'https://github.com/jordibartolome',
   },
   // {
   // 	className: "fa fa-fw fa-twitter",
   // 	link: "https://www.twitter.com/jordibartolome"
   // },
   {
-    className: "fa fa-fw fa-youtube",
-    link: "https://www.youtube.com/user/tubasu"
-  }
+    className: 'fa fa-fw fa-youtube',
+    link: 'https://www.youtube.com/user/tubasu',
+  },
   // {
   // 	className: "fa fa-fw fa-google-plus",
   // 	link: "https://plus.google.com/+JordiBartolom%C3%A9Guill%C3%A9n/posts"
